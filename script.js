0c9f6ca331c4fa4641e9c4fce34e075de4e82f03
Rafael Kataoka <rafaelktk2005@gmail.com>
2026-05-30 02:26:17 -0300

    Cars website

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let prevButton = document.getElementById("prev")
+let nextButton = document.getElementById("next")
+let container = document.querySelector(".container")
+let item = container.querySelectorAll(".list .item")
+let indicators = document.querySelector(".indicators")
+let dots = indicators.querySelectorAll("ul li")
+let lista = container.querySelector(".list")
+
+let active = 0
+let firstPosition = 0
+let lastPosition = item.length - 1
+
+
+function setSlider(){
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove("active")
+
+    indicators.querySelector(".numbers").innerHTML = "0" + ( active + 1)
+
+}
+
+nextButton.onclick = () => {
+    lista.style.setProperty("--calculation", 1)
+    active = active + 1 > lastPosition ? 0 : active + 1
+    setSlider()
+    item[active].classList.add("active")
+
+    
+}
+
+prevButton.onclick = () => {
+    lista.style.setProperty("--calculation", -1)
+    active = active - 1 < firstPosition ? lastPosition : active - 1
+    setSlider()
+    item[active].classList.add("active")
+
+}
